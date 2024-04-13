8612f38716de4a862f035ac35991385fb7efa37f
Yoan HOSTIN <hostinyoan76@gmail.com>
2024-04-13 06:39:07 +0200

    uhjbjlm

---
+++ app/src/main/java/org/libreapps/rest/Commander.java
@@ -0,0 +1,29 @@
+package org.libreapps.rest;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.libreapps.rest.obj.Product;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+public class Commander extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commander);
+    }
+}
--- app/src/main/java/org/libreapps/rest/WelcomePage.java
+++ app/src/main/java/org/libreapps/rest/WelcomePage.java
@@ -1,6 +1,7 @@
 package org.libreapps.rest;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import org.libreapps.rest.obj.Product;
@@ -18,10 +20,23 @@ import org.libreapps.rest.obj.Product;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 public class WelcomePage extends AppCompatActivity {
-
+    private Button buttonCommander;
+    private Button buttonHistorique;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomepage);
+        buttonCommander = (Button)findViewById(R.id.button_Commander);
+        buttonHistorique = (Button)findViewById(R.id.button_Historique);
+        buttonCommander.setBackgroundColor(Color.BLACK);
+        buttonHistorique.setBackgroundColor(Color.BLACK);
+
+        buttonCommander.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, Commander.class);
+                startActivity(intent);
+            }
+        });
     }
 }
